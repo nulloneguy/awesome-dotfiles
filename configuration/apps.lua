1838f376d0a560097e710dfeea1c26b2a72ae4e4
nulloneguy <nulloneguy@ya.ru>
2022-08-07 12:20:21 +0700

    update polkit

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -65,7 +65,7 @@ return {
 		-- Load X colors
 		"xrdb $HOME/.Xresources",
 		-- Polkit and keyring
-		"/usr/lib/polkit-kde-authentication-agent-1 &" .. " eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)",
+		"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &" .. " eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)",
 		-- Music server
 		"mpd",
 		-- Lockscreen timer
